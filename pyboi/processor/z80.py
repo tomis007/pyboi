651c3b2065c4f11a7636ce6ee176f4988d7f883b
tomis007 <thomas.hendrickson@tufts.edu>
2017-03-07 15:43:48 +0000

    adding opcodes

--- pyboi/processor/z80.py
+++ pyboi/processor/z80.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, PickleType
 from ..base import Base
+from ctypes import c_int8
+from enum import Enum
 import pickle
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -78,11 +80,16 @@ class Z80():
         self.F = 5
         self.H = 6
         self.L = 7
-        self.HL = 8
-        self.N = 9 # immediate load value
-        self.NN = 10
-        self.BC = 11
-        self.DE = 12
+        # register enums
+        self.reg_pairs = Enum('RegPairs', 'HL BC DE AF')
+        self.load_vals = Enum('ImmediateByte', 'N NN')
+        self.HL = self.reg_pairs.HL
+        self.BC = self.reg_pairs.BC
+        self.DE = self.reg_pairs.DE
+        self.AF = self.reg_pairs.AF
+        self.N = self.load_vals.N
+        self.NN = self.load_vals.NN
+        self.flags = Enum('Flags', 'Z N H C')
         #pc/sp
         self.pc = 0x100
         self.sp = 0xfffe
@@ -173,7 +180,57 @@ class Z80():
             0x2a: lambda: self.load_a_hl(dec=False, load=True),
             0x22: lambda: self.load_a_hl(dec=False, load=False),
             0xe0: lambda: self.a_n(store=True),
-            0xf0: lambda: self.a_n(store=False)
+            0xf0: lambda: self.a_n(store=False),
+            0x01: lambda: self.ld_nn(self.BC, set_sp=False),
+            0x11: lambda: self.ld_nn(self.DE, set_sp=False),
+            0x21: lambda: self.ld_nn(self.HL, set_sp=False),
+            0x31: lambda: self.ld_nn(self.sp, set_sp=True),
+            0xf9: lambda: self.ld_sp_hl(),
+            0xf8: lambda: self.ldhl_sp(),
+            0x08: lambda: self.ld_nn_sp(),
+            0xf5: lambda: self.push_nn(self.AF),
+            0xc5: lambda: self.push_nn(self.BC),
+            0xd5: lambda: self.push_nn(self.DE),
+            0xe5: lambda: self.push_nn(self.HL),
+            0xf1: lambda: self.pop_nn(self.AF),
+            0xc1: lambda: self.pop_nn(self.BC),
+            0xd1: lambda: self.pop_nn(self.DC),
+            0xe1: lambda: self.pop_nn(self.HL),
+            0x87: lambda: self.add_a_n(self.A, add_carry=False),
+            0x80: lambda: self.add_a_n(self.B, add_carry=False),
+            0x81: lambda: self.add_a_n(self.C, add_carry=False),
+            0x82: lambda: self.add_a_n(self.D, add_carry=False),
+            0x83: lambda: self.add_a_n(self.E, add_carry=False),
+            0x84: lambda: self.add_a_n(self.H, add_carry=False),
+            0x85: lambda: self.add_a_n(self.L, add_carry=False),
+            0x86: lambda: self.add_a_n(self.HL, add_carry=False),
+            0xc6: lambda: self.add_a_n(self.N, add_carry=False),
+            0x8f: lambda: self.add_a_n(self.A, add_carry=True),
+            0x88: lambda: self.add_a_n(self.B, add_carry=True),
+            0x89: lambda: self.add_a_n(self.C, add_carry=True),
+            0x8a: lambda: self.add_a_n(self.D, add_carry=True),
+            0x8b: lambda: self.add_a_n(self.E, add_carry=True),
+            0x8c: lambda: self.add_a_n(self.H, add_carry=True),
+            0x8d: lambda: self.add_a_n(self.L, add_carry=True),
+            0x8e: lambda: self.add_a_n(self.HL, add_carry=True),
+            0xce: lambda: self.add_a_n(self.N, add_carry=True),
+            0x97: lambda: self.sub_a_n(self.A, sub_carry=False),
+            0x90: lambda: self.sub_a_n(self.B, sub_carry=False),
+            0x91: lambda: self.sub_a_n(self.C, sub_carry=False),
+            0x92: lambda: self.sub_a_n(self.D, sub_carry=False),
+            0x93: lambda: self.sub_a_n(self.E, sub_carry=False),
+            0x94: lambda: self.sub_a_n(self.H, sub_carry=False),
+            0x95: lambda: self.sub_a_n(self.L, sub_carry=False),
+            0x96: lambda: self.sub_a_n(self.HL, sub_carry=False),
+            0xd6: lambda: self.sub_a_n(self.N, sub_carry=False),
+            0x9f: lambda: self.sub_a_n(self.A, sub_carry=True),
+            0x98: lambda: self.sub_a_n(self.B, sub_carry=True),
+            0x99: lambda: self.sub_a_n(self.C, sub_carry=True),
+            0x9a: lambda: self.sub_a_n(self.D, sub_carry=True),
+            0x9b: lambda: self.sub_a_n(self.E, sub_carry=True),
+            0x9c: lambda: self.sub_a_n(self.H, sub_carry=True),
+            0x9d: lambda: self.sub_a_n(self.L, sub_carry=True),
+            0x9e: lambda: self.sub_a_n(self.HL, sub_carry=True)
         }
 
     def save_state(self, name, session):
@@ -296,7 +353,7 @@ class Z80():
             self.reg[self.A] = self.mem.read(self.mem.read_word(self.pc))
             self.pc += 2
             return 16
-        else:
+        else: #self.N
             self.reg[self.A] = self.mem.read(self.pc)
             self.pc += 1
             return 8
@@ -375,8 +432,7 @@ class Z80():
             self.mem.write(self.reg[self.A], self.get_reg(self.H, self.L))
         HL_val = self.get_reg(self.H, self.L)
         HL_val += -1 if dec else 1
-        self.reg[self.H] = (HL_val & 0xff00) >> 8
-        self.reg[self.L] = HL_val & 0xff
+        self.set_reg(self.H, self.L, HL_val)
         return 8
 
     def a_n(self, store=False):
@@ -399,6 +455,140 @@ class Z80():
         self.pc += 1
         return 12
 
+    def ld_nn(self, dest, set_sp=False):
+        """
+        Put value nn into dest.
+
+        Dest = BC/DE/HL/SP
+
+        Parameters
+        ----------
+        dest : int
+            destination register pair (defined in class constants)
+            if not self.BC/DE/HL defaults to setting stack pointer
+        set_sp : bool
+            if True, loads value into stack pointer
+            if False, doesnt
+
+        Returns
+        -------
+        int 
+            num of cycles
+        """
+        word = self.mem.read_word(self.pc)
+        self.pc += 2
+        if set_sp:
+            self.sp = word
+            return 12
+        elif dest == self.BC:
+            r1 = self.B
+            r2 = self.C
+        elif dest == self.DE:
+            r1 = self.D
+            r2 = self.E
+        elif dest == self.HL:
+            r1 = self.H
+            r2 = self.L
+        self.set(r1, r2, word)
+        return 12
+
+    def ld_sp_hl(self):
+        """
+        Put HL into sp.
+
+        Returns
+        -------
+        int
+            number of cycles
+        """
+        self.sp = self.get_reg(self.H, self.L)
+        return 8
+
+    def ldhl_sp(self):
+        """
+        Put sp + n effective address into HL.
+        n = one byte signed value
+        
+        Flags:
+        Z/N - Reset
+        H/C - Set/Reset according to operation
+        """
+        #interpret as signed byte
+        n = c_int8(self.mem.read(self.sp)).value
+        self.set_reg(self.H, self.L, self.sp + n)
+
+        self.reset_flags()
+        if (self.sp & 0xf) + (n & 0xf) > 0xf:
+            self.set_flag(self.flags.H)
+        if (self.sp & 0xff) + (n & 0xff) > 0xff:
+            self.set_flag(self.flags.C)
+        return 12
+
+    def ld_nn_sp(self):
+        """
+        Put sp at address nn (two byte immediate address).
+        
+        Returns
+        -------
+        int
+            number of clock cycles
+        """
+        address = self.mem.read_word(self.pc)
+        self.pc += 2
+        self.mem.write(self.sp & 0xff, address)
+        self.mem.write((self.sp & 0xff00) >> 8, address + 1)
+        return 20
+
+
+    def push_nn(self, r1, r2):
+        """
+        Push register pair r1r2 onto stack.
+        Decrement sp twice.
+
+        Parameters
+        ----------
+        r1, r2
+            register pair r1r2
+        """
+        self.sp -= 1
+        self.mem.write(self.reg[r1], self.sp)
+        self.sp -= 1
+        self.mem.write(self.reg[r2], self.sp)
+        return 16
+
+    def pop_nn(self, r1, r2):
+        """
+        Pop two bytes off stack into register pair r1r2.
+        Increment sp twice.
+
+        Parameters
+        ----------
+            r1 
+                reg1
+            r2
+                reg2
+        """
+        self.reg[r2] = self.mem.read(self.sp)
+        self.sp += 1
+        self.reg[r1] = self.mem.read(self.sp)
+        self.sp += 1
+        return 12
+
+
+    def set_reg(self, r1, r2, word):
+        """
+        set register pair r1r2 to 16 bit word.
+
+        Parameters
+        ----------
+        r1,r2 : ints
+            indexes of r1 r2 registers to set
+            r1 = H, r2 = L sets pair HL
+        """
+        self.reg[r1] = (word & 0xff00) >> 8
+        self.reg[r2] = word & 0xff
+        
+
     def get_reg(self, r1, r2):
         """
         Access register r1r2 - combination of r1 and r1 registers.
@@ -411,4 +601,154 @@ class Z80():
             value of HL register
         """
         return ((self.reg[r1] << 8) | self.reg[r2]) 
+
+
+    def set_flag(self, flag):
+        """
+        Sets Flag flag in the F register.
+
+        Parameters
+        ----------
+        flag : Flag enum
+            which flag to set
+        """
+        if flag == self.flags.Z:
+            self.reg[self.F] |= 0x80
+        elif flag == self.flags.H:
+            self.reg[self.F] |= 0x20
+        elif flag == self.flags.C:
+            self.reg[self.F] |= 0x10
+        elif flag == self.flags.N:
+            self.reg[self.F] |= 0x40
+
+    def reset_flag(self, flag):
+        """
+        Resets Flag flag in the F register.
+
+        Parameters
+        ----------
+        flag : Flag enum
+            which flag to reset
+        """
+        if flag == self.flags.Z:
+            self.reg[self.F] &= 0x70
+        elif flag == self.flags.H:
+            self.reg[self.F] &= 0xd0
+        elif flag == self.flags.C:
+            self.reg[self.F] &= 0xe0
+        elif flag == self.flags.N:
+            self.reg[self.F] &= 0xb0
+
+    def flag_set(self, flag):
+        """
+        Returns True if flag is set
+        False if not
+
+        Parameters
+        ----------
+        flag : Flag enum
+            which flag to check
+
+        Returns
+        -------
+        bool
+            True if set, False if not
+        """
+        if flag == self.flags.Z:
+            return self.reg[self.F] & 0x80 != 0
+        elif flag == self.flags.H:
+            return self.reg[self.F] & 0x20 != 0
+        elif flag == self.flags.C:
+            return self.reg[self.F] & 0x10 != 0
+        elif flag == self.flags.N:
+            return self.reg[self.F] & 0x40 != 0
+
+    def add_a_n(self, src, add_carry=False):
+        """
+        Add n to A (and carry if add_carry is true).
+        Flags: 
+        Z - Set if zero
+        N - Reset
+        H - Set if carry from bit 3
+        C - Set if carry from bit 7
+        
+        Parameters
+        ----------
+        src
+            source A-L, (HL), or n
+
+        Returns
+        -------
+        int 
+            clock cycles taken
+        """
+        a_reg = self.reg[self.A]
+        if src == self.N:
+            val = self.mem.read(self.pc)
+            self.pc += 1
+        elif src == self.HL:
+            val = self.mem.read(self.get_reg(self.H, self.L))
+        else: #src is index of A-L
+            val = self.reg[src]
+        carry_bit = 1 if add_carry and flag_set(self.flags.C) else 0
+        
+        self.reg[self.A] = (a_reg + carry_bit + val) & 0xff
+        self.reset_flags()
+        if self.reg[self.A] == 0:
+            self.set_flag(self.flags.Z)
+        if (a_reg & 0xf) + (val & 0xf) + carry_bit > 0xf:
+            self.set_flag(self.flags.H)
+        if a_reg + val + carry_bit > 0xff:
+            self.set_flag(self.flags.C)
+
+        return 8 if src == self.N or src == self.HL else 4
+
+    def sub_a_n(self, src, sub_carry=False):
+        """
+        Subtract n from A (n + carry if sub_carry is true)
+        Flags:
+        Z - Set if 0
+        N - Set
+        H - Set if no borrow from bit 4
+        C - Set if no borrow
+
+        Parameters
+        ----------
+        src
+            source A-L, (HL), or n
+        Returns
+        -------
+        int 
+            number of cylces elapsed
+        """
+        a_reg = self.reg[self.A]
+        if src == self.N:
+            val = self.mem.read(self.pc)
+            self.pc += 1
+        elif src == self.HL:
+            val = self.mem.read(self.get_reg(self.H, self.L))
+        else: #src is index of A-L
+            val = self.reg[src]
+        carry_bit = 1 if sub_carry and flag_set(self.flags.C) else 0
+
+        self.reg[self.A] = (a_reg - val - carry_bit) & 0xff
+        self.reset_flags()
+        if self.reg[self.A] == 0:
+            self.set_flag(self.flags.Z)
+        if (a_reg & 0xf) < (val & 0xf) + carry_bit:
+            self.set_flag(self.flags.H)
+        if a_reg < val + carry_bit:
+            self.set_flag(self.flags.C)
+
+        return 8 if src == self.N or src == self.HL else 4
+
+
+
+
+
+
+    def reset_flags(self):
+        """ Resets all Flags to 0.  """
+        self.reg[self.F] = 0
+
 
