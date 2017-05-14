0e1068d06e6e98bab1c1782182d58e06da4f19c2
tomis007 <thomas.hendrickson@tufts.edu>
2017-05-14 09:32:36 +0100

    added initial background rendering

--- pyboi/gpu/gpu.py
+++ pyboi/gpu/gpu.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from ctypes import c_int8
 import logging
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(name='gpu')
@@ -79,8 +80,7 @@ class GPU:
             else:
                 self.scanline += 1
                 self.mem.write(self.scanline, 0xff44)
-                self.set_mode(self.modes.VB, self.mode_clock % 456)
-            
+                self.set_mode(self.modes.VB, self.mode_clock % 456) 
 
     #TODO block OAM memory access
     def o_ram(self, cycles):
@@ -138,9 +138,17 @@ class GPU:
 
        
     def draw_scanline(self, scanline):
-        #print("drawing background " + str(scanline))
+        """
+        Draws the scanline specified based on the current
+        contents of memory
+
+        ...
+        Parameters
+        ----------
+        scanline : int
+            scanline to draw (0-143)
+        """
         self.draw_background(scanline)
-        #self.mem.write(self.scanline, 0xff44)
 
 
     def draw_background(self, scanline):
@@ -153,16 +161,94 @@ class GPU:
             to draw background on
         """
         # background display register
-        if not self.is_set(0xff40, 0):
-            #print("background not turned on :(")
+        lcd_control = self.mem.read(0xff40)
+        if not self.bit_set(lcd_control, 0):
             return #bg turned off
-        else:
-            #print("DRAWING BACKGROUND!!!!!!!")
-            pass
 
+        #print("drawing bg scanline: " + str(scanline))
+        scY = self.mem.read(0xff42)
+        scX = self.mem.read(0xff43)
+        tile_select = 0x9000 if self.bit_set(lcd_control, 4) else 0x8000
+        tile_map = 0x9c00 if self.bit_set(lcd_control, 3) else 0x9800
+
+        y_offset = (((scY + scanline) // 8) % 32) * 32
+        tile_line = (scY + scanline) % 8
+
+        for x_tile in range(21):
+            x_offset = (x_tile + (scX // 8)) % 32
+            tile_index = self.mem.read(tile_map + y_offset + x_offset)
+            if tile_select == 0x9000:
+                tile_index = c_int8(tile_index).value
+            address = tile_select + (16 * tile_index)
+            x_shift = scX % 8
+            if x_tile == 0 and x_shift != 0:
+                self.draw_bg_tile(address, tile_line, 0, scanline, x_shift, 7)
+            elif x_tile == 20 and x_shift != 0:
+                self.draw_bg_tile(address, tile_line, 160 - x_shift, scanline, 0, x_shift - 1)
+            else:
+                self.draw_bg_tile(address, tile_line, (x_tile * 8) - x_shift, scanline, 0, 7)
+                
+
+    def draw_bg_tile(self, address, line, x, y, pix_start, pix_end):
+        """
+        Draws a background tile into the display buffer.
+
+        ...
+        Parameters
+        ----------
+        address : int
+            address in memory of tile data 
+        line : int
+            line of the tile to draw (0-7)
+        x : int
+            x position on the GB screen
+        y : int
+            y position on the GB screen
+        pix_start : int
+            first pixel in the block to draw
+        pix_end : int
+            last pixel in the block to draw
+        """
+        block_one = self.mem.read(address + (2 * line))
+        block_two = self.mem.read(address + (2 * line) + 1)
+
+        #TODO check if loops correctly
+        for pixel in range(pix_start, pix_end):
+            color = self.get_color(block_one, block_two, pixel)
+            x_pix = (x + pixel - pix_start) % 160
+            self.draw_to_buffer(x_pix, y, color)
+
+    def draw_to_buffer(self, col, row, color):
+        """
+        Places color at position col,row in the screen buffer.
+        color is a byte value
+        """
+        if color != 0:
+            print("DRAWING TO BUFFER: " + str(color))
+        self.gb_screen[col + (row * 160)] = color
+
+    def get_color(self, block_one, block_two, pixel):
+        """
+        Return the color of the pixel to draw.
+        For GB MODE
+        0-3
+        """
+        pix = 7 - pixel
+        color_num = 1 if self.bit_set(block_two, pix) else 0
+        color_num = (color_num << 1) | (1 if self.bit_set(block_one, pix) else 0)
+        palette = self.mem.read(0xff47)
+        color = (palette >> (color_num * 2)) & 0x3
+
+        return color
 
         
-
+    def bit_set(self, data, bit):
+        """
+        Checks if bit bit is set in data.
+        Same as is_set without reading
+        from memory
+        """
+        return True if (data >> bit) & 0x1 == 0x1 else False
 
     def is_set(self, address, bit):
         """
@@ -197,3 +283,7 @@ class GPU:
         bool
         """
         return True if (self.mem.read(0xff40) & 0x80) == 0x80 else False
+
+    def get_frame_buffer(self):
+        """ Returns the current gb screen buffer. """
+        return self.gb_screen
